bb9f7285fca0e60a3811da092100efa2761d4c35
Hritvik Soni <hritviksoni768@gmail.com>
2024-02-07 12:15:09 +0530

    addded

---
+++ src/main/java/com/WUE/WhatsAppCommunicationWUE/WhatsAppCommunicationWueApplication.java
@@ -0,0 +1,13 @@
+package com.WUE.WhatsAppCommunicationWUE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsAppCommunicationWueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsAppCommunicationWueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WUE/WhatsAppCommunicationWUE/WhatsAppCommunicationWueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WUE.WhatsAppCommunicationWUE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppCommunicationWueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
